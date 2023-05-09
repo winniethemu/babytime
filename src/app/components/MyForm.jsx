0651e4a9b639643b804faf6d4b28e73d6ce90dd3
Mu Ye <emailyemu@gmail.com>
2023-05-09 14:33:26 +0800

    Style fix

--- src/app/components/MyForm.jsx
+++ src/app/components/MyForm.jsx
@@ -27,40 +27,46 @@ export default function MyForm({ submit }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <FormControl fullWidth className={styles.formControl}>
-        <InputLabel id="activity-label">Activity</InputLabel>
-        <Select
-          labelId='activity-label'
-          value={activity}
-          label="Activity"
-          onChange={(e) => setActivity(e.target.value)}
-        >
-          <MenuItem value={EVENT_TYPE.feed}>Feed</MenuItem>
-          <MenuItem value={EVENT_TYPE.nappy1}>Nappy One</MenuItem>
-          <MenuItem value={EVENT_TYPE.nappy2}>Nappy Two</MenuItem>
-          <MenuItem value={EVENT_TYPE.spit}>Spit</MenuItem>
-          <MenuItem value={EVENT_TYPE.tummy}>Tummy</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl fullWidth className={styles.formControl}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateTimePicker
-            label="Start Time"
-            value={startTime}
-            onChange={(dt) => setStartTime(dt.valueOf())}
-          />
-        </LocalizationProvider>
-      </FormControl>
-      <FormControl fullWidth className={styles.formControl}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateTimePicker
-            label="End Time"
-            value={endTime}
-            onChange={(dt) => setEndTime(dt.valueOf())}
-            disabled={activity !== EVENT_TYPE.feed}
-          />
-        </LocalizationProvider>
-      </FormControl>
+      <div className={styles.formControl}>
+        <FormControl fullWidth>
+          <InputLabel id="activity-label">Activity</InputLabel>
+          <Select
+            labelId='activity-label'
+            value={activity}
+            label="Activity"
+            onChange={(e) => setActivity(e.target.value)}
+          >
+            <MenuItem value={EVENT_TYPE.feed}>Feed</MenuItem>
+            <MenuItem value={EVENT_TYPE.nappy1}>Nappy One</MenuItem>
+            <MenuItem value={EVENT_TYPE.nappy2}>Nappy Two</MenuItem>
+            <MenuItem value={EVENT_TYPE.spit}>Spit</MenuItem>
+            <MenuItem value={EVENT_TYPE.tummy}>Tummy</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className={styles.formControl}>
+        <FormControl fullWidth>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateTimePicker
+              label="Start Time"
+              value={startTime}
+              onChange={(dt) => setStartTime(dt.valueOf())}
+            />
+          </LocalizationProvider>
+        </FormControl>
+      </div>
+      <div className={styles.formControl}>
+        <FormControl fullWidth>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateTimePicker
+              label="End Time"
+              value={endTime}
+              onChange={(dt) => setEndTime(dt.valueOf())}
+              disabled={activity !== EVENT_TYPE.feed}
+            />
+          </LocalizationProvider>
+        </FormControl>
+      </div>
       <Box className={styles.formSubmit}>
         <Button
           variant="contained"
