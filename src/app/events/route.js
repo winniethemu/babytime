718d34ea44cf33dcc161b4228b01df57d38e7896
Mu Ye <emailyemu@gmail.com>
2023-05-17 11:48:19 +0800

    GET only fetches recent data

--- src/app/events/route.js
+++ src/app/events/route.js
@@ -3,20 +3,24 @@ import { sql } from '@vercel/postgres';
 
 export async function GET() {
   if (process.env.VERCEL_ENV === 'development') {
-    const { rows } = await sql`SELECT * from events_test ORDER BY time DESC`;
+    const { rows } = await sql`
+      SELECT * from events_test WHERE time > extract(epoch from (now() - interval '2 day')) * 1000 ORDER BY time DESC
+    `;
     return NextResponse.json({ data: rows });
   } else {
-    const { rows } = await sql`SELECT * from events ORDER BY time DESC`;
+    const { rows } = await sql`
+      SELECT * from events WHERE time > extract(epoch from (now() - interval '2 day')) * 1000 ORDER BY time DESC
+    `;
     return NextResponse.json({ data: rows });
   }
 }
 
-export async function POST(request) {
-  const res = await request.json();
+export async function POST(req) {
+  const json = await req.json();
   if (process.env.VERCEL_ENV === 'development') {
-    await sql`INSERT INTO events_test (type, time) VALUES (${res.type}, ${res.time})`;
+    await sql`INSERT INTO events_test (type, time) VALUES (${json.type}, ${json.time})`;
   } else {
-    await sql`INSERT INTO events (type, time) VALUES (${res.type}, ${res.time})`;
+    await sql`INSERT INTO events (type, time) VALUES (${json.type}, ${json.time})`;
   }
-  return NextResponse.json({ res });
+  return new NextResponse();
 }
